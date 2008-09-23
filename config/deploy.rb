aece56c3e485198fbe7ef2c435a3abfc2f6264f2
Ben Hughes <bwghughes@gmail.com>
2008-09-23 15:01:33 +0100

    Added deployment information. Signed-off-by: Ben Hughes <bwghughes@gmail.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,19 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
-
+set :application, "Testing Script Capture"
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/var/www/webapps/testing-app"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-# set :scm, :subversion
+set :scm, :git
+set :repository,  "git://github.com/bwghughes/testing.git"
+set :scm_passphrase, "icbiatwt2"
+set :user, "bwghughes"
+set :branch, "master"
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+# Machines
+set :user, "ben"
+role :app, "10.11.10.162"
+role :web, "10.11.10.162"
+role :db,  "10.11.10.162", :primary => true
